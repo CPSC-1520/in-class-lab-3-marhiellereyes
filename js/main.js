08223de5124544e680ea1c5697ae62726c58cef8
marhiellereyes <mbreyes@hotmail.ca>
2024-10-02 21:46:57 -0600

    added evenListener for click, mouseover, and mouseout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 // Enter your code below.
+const resourcesDiv = document.querySelector('.javascript-resources');
+const showResourcesButton = document.getElementById
+('show-resources');
+
+showResourcesButton.addEventListener('click',() => {
+    resourcesDiv.classList.remove('d-none');
+
+});
+
+resourcesDiv.addEventListener('mouseover', (event) => {
+    if (event.target.classList.contains('list-group-item')) {
+        event.target.classList.remove('fw-bold');
+
+    }
+    event.target.classList.add('fw-bold');
+});
+
+resourcesDiv.addEventListener('mouseout', (event) => {
+    if (event.target.classList.contains('list-group-item')) {
+        event.target.classList.remove('fw-bold');
+    }
+});
+
+resourcesDiv.addEventListener('click', (event) => {
+    if (event.target.classList.contains('list-group-item')) {
+        event.target.classList.remove('fw-bold');
+
+    }
+    event.target.classList.add('fst-italic');
+
+});
